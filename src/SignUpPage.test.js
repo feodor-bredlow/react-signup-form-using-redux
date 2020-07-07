1e23df73c17698328fb6bd517d18316970202ef2
Feodor Tobler <feodor-tobler>
2020-07-07 16:49:00 +0200

    Initialize SignUpPage main component

--- src/SignUpPage.test.js
+++ src/SignUpPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import SignUpPage from './SignUpPage';
 
 test('renders learn react link', () => {
-	const { getByText } = render(<App />);
+	const { getByText } = render(<SignUpPage />);
 	const linkElement = getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SignUpPage from './SignUpPage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<SignUpPage />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
