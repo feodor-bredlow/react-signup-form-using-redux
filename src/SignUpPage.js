55cc8a720c51235bf3f1218fa6c80bb37ae46859
Feodor Tobler <feodor-tobler>
2020-07-07 17:27:33 +0200

    Add wrapper around content in main component SignUpPage

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './SignUpPage.module.scss';
 
 const SignUpPage = () => {
-	return <div>HELLO WORLD</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.content}>Hello World</div>
+		</div>
+	);
 };
 
 export default SignUpPage;
