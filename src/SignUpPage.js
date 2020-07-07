81b896825f839e9651ac62f96dc28a149cf3c32f
Feodor Tobler <feodor-tobler>
2020-07-07 19:51:53 +0200

    Add all input fields to main component

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SignUpPage.module.scss';
 import DefaultForm from './components/DefaultForm/DefaultForm.js';
 import DefaultInput from './components/DefaultInput/DefaultInput.js';
+import CheckboxInput from './components/CheckboxInput/CheckboxInput.js';
 
 const SignUpPage = () => {
+	const [showAddress, setShowAddress] = useState(true);
+
 	return (
 		<div className={styles.wrapper}>
 			<DefaultForm>
-				<DefaultInput label="Vorname" />
+				<DefaultInput label="First name" />
+				<DefaultInput label="Last name" />
+				<DefaultInput label="Nick name" />
+				<DefaultInput label="Email" />
+				<DefaultInput label="Password" />
+				<DefaultInput label="Password (repeat)" />
+				<CheckboxInput
+					label="Show address"
+					setShowAddress={setShowAddress}
+					checked={showAddress}
+				/>
+				{showAddress && (
+					<>
+						<DefaultInput label="Street" />
+						<DefaultInput label="House number" />
+						<DefaultInput label="ZIP" />
+						<DefaultInput label="City" />
+					</>
+				)}
+				<DefaultInput label="Additional information" multiLine />
 			</DefaultForm>
 		</div>
 	);
