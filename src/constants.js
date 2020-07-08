db85265055fdaf6123b8723dd828cd5fff64e4f0
Feodor Tobler <feodor-tobler>
2020-07-08 09:07:15 +0200

    Create constants file for the naming of the store

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const USER_INFO = {
+	FIRST_NAME: 'firstName',
+	LAST_NAME: 'lastName',
+	NICK_NAME: 'nickName',
+	EMAIL: 'email',
+	PASSWORD: 'password',
+	REPEAT_PASSWORD: 'repeatPassword',
+	SHOW_ADDRESS: 'showAddress',
+	STREET: 'street',
+	HOUSE_NR: 'houseNr',
+	ZIP: 'zip',
+	CITY: 'city',
+	ADDITIONAL_INFO: 'additionalInfo',
+};
