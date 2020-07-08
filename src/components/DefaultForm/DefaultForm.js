5955b82aa4599d2f0dfc303f876d565fa74c53ab
Feodor Tobler <feodor-tobler>
2020-07-08 11:53:11 +0200

    connect submit button to submit form validation function isSubmitDisabled

--- src/components/DefaultForm/DefaultForm.js
+++ src/components/DefaultForm/DefaultForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import styles from './DefaultForm.module.scss';
+import * as validateInput from '../../utils/validateInput';
 
 const DefaultForm = (props) => {
 	const handleFormSubmit = (event) => {
@@ -15,6 +17,7 @@ const DefaultForm = (props) => {
 				type="submit"
 				value="Submit"
 				className={styles.submitButton}
+				disabled={validateInput.isSubmitDisabled(props.userInfo)}
 			/>
 		</form>
 	);
@@ -22,6 +25,11 @@ const DefaultForm = (props) => {
 
 DefaultForm.propTypes = {
 	children: PropTypes.any,
+	userInfo: PropTypes.object,
 };
 
-export default DefaultForm;
+const mapStateToProps = (state) => {
+	return { userInfo: state.userInfo };
+};
+
+export default connect(mapStateToProps)(DefaultForm);
